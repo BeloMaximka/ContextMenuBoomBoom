9b6ecfbc15b8f61465f3f90eedf382eff6cfc17d
BeloMaximka <belomaxim@gmail.com>
2022-09-09 10:35:55 +0300

    Добавьте файлы проекта.

---
+++ ContextMenuBoomBoom/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ContextMenuBoomBoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.buttonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buttonToolStripMenuItem,
+            this.textBoxToolStripMenuItem,
+            this.checkBoxToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 92);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // buttonToolStripMenuItem
+            // 
+            this.buttonToolStripMenuItem.Name = "buttonToolStripMenuItem";
+            this.buttonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.buttonToolStripMenuItem.Text = "Кнопка";
+            this.buttonToolStripMenuItem.Click += new System.EventHandler(this.buttonToolStripMenuItem_Click);
+            // 
+            // textBoxToolStripMenuItem
+            // 
+            this.textBoxToolStripMenuItem.Name = "textBoxToolStripMenuItem";
+            this.textBoxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.textBoxToolStripMenuItem.Text = "Текстовое поле";
+            this.textBoxToolStripMenuItem.Click += new System.EventHandler(this.textBoxToolStripMenuItem_Click);
+            // 
+            // checkBoxToolStripMenuItem
+            // 
+            this.checkBoxToolStripMenuItem.Name = "checkBoxToolStripMenuItem";
+            this.checkBoxToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkBoxToolStripMenuItem.Text = "Чекбокс";
+            this.checkBoxToolStripMenuItem.Click += new System.EventHandler(this.checkBoxToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem buttonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkBoxToolStripMenuItem;
+    }
+}
+
---
+++ ContextMenuBoomBoom/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ContextMenuBoomBoom
+{
+    public partial class Form1 : Form
+    {
+        int limit;
+        Point location;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            limit = 3;
+            buttonToolStripMenuItem.Text = $"{buttonToolStripMenuItem.Text} ({limit})";
+            textBoxToolStripMenuItem.Text = $"{textBoxToolStripMenuItem.Text} ({limit})";
+            checkBoxToolStripMenuItem.Text = $"{checkBoxToolStripMenuItem.Text} ({limit})";
+        }
+
+        void CreateControl(ToolStripMenuItem item, Control toCreate)
+        {
+            location.X -= Location.X + toCreate.Width / 2;
+            location.Y -= Location.Y + this.Height - this.ClientRectangle.Height;
+            toCreate.Location = location;
+            Controls.Add(toCreate);
+
+            int count = int.Parse(Regex.Match(item.Text, @"\d+").Value);
+            count--;
+            item.Text = Regex.Replace(item.Text, @"\d+", count.ToString());
+
+            if (count == 0)
+                contextMenuStrip1.Items.Remove(item);
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            location = Control.MousePosition;
+        }
+
+        private void buttonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateControl(sender as ToolStripMenuItem, new Button());
+        }
+
+        private void textBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateControl(sender as ToolStripMenuItem, new TextBox());
+        }
+
+        private void checkBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateControl(sender as ToolStripMenuItem, new CheckBox());
+        }
+    }
+}
